5c785c89ba9b7662c6e223ec9ab9c635aab40eff
Onurv77 <ozcanonur1388@hotmail.com>
2024-12-08 17:22:16 +0300

    deneme4

--- app/src/main/java/com/example/santra/MainActivity.kt
+++ app/src/main/java/com/example/santra/MainActivity.kt
@@ -56,7 +56,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import kotlinx.coroutines.launch
 
-
+//Main Activity new arch
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
