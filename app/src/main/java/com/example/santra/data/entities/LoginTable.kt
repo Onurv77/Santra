1a58dfaf85b9547d6de4165e4e6ff8e088afc8e6
kadirtilikoglu <kadirtilikoglu1@gmail.com>
2024-12-15 12:36:53 +0300

    Register insert

--- app/src/main/java/com/example/santra/data/entities/LoginTable.kt
+++ app/src/main/java/com/example/santra/data/entities/LoginTable.kt
@@ -6,12 +6,10 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class LoginTable(
+    @PrimaryKey(autoGenerate = true ) val StudentID: Int = 0,
     @ColumnInfo(name = "StudentNumber") val StudentNumber: String,
     @ColumnInfo(name = "StudentMail") val StudentMail: String,
     @ColumnInfo(name = "StudentPassword") val StudentPassword: String
 
-) {
-    @PrimaryKey(autoGenerate = true)
-    var id = 0
-}
+)
 
