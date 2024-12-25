457685c13ec17bfcdca7b99373a8e9d255facc13
Onurv77 <ozcanonur1388@hotmail.com>
2024-12-25 15:03:13 +0300

    ilandetayiguncellendi

--- app/src/main/java/com/example/santra/ui/components/LazyColumnContent.kt
+++ app/src/main/java/com/example/santra/ui/components/LazyColumnContent.kt
@@ -1,32 +1,67 @@
 package com.example.santra.ui.components
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
+//noinspection UsingMaterialAndMaterial3Libraries
+import androidx.compose.material.Card
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.santra.R
 
 @Composable
-fun LazyColumnContent(
-    modifier: Modifier = Modifier,
-    verticalArrangement: Arrangement.HorizontalOrVertical,
-    navController: NavController
-) {
+fun LazyRowContent(navController: NavController) {
 
-    LazyColumn(
-        modifier = modifier.fillMaxWidth(),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
+    LazyRow(
+        modifier = Modifier.fillMaxWidth()
     ) {
         items(5) { index ->
-            Text(
+
+            Card(
+                modifier = Modifier.padding(end = 10.dp)
+                    .fillMaxWidth()
+                    .clickable { navController.navigate("announcement_detail/$index") },
+                elevation = 8.dp,
+                shape = RoundedCornerShape(10.dp),
+            ) {
+                Column(
+                    modifier = Modifier.padding(16.dp)
+                ) {
+                    Image(
+                        contentDescription = null,
+                        painter = painterResource(R.drawable.account_circle),
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                            .height(125.dp)
+                            .padding(bottom = 5.dp)
+                    )
+                    Text(text = "Username", style = MaterialTheme.typography.headlineSmall, modifier = Modifier
+                        .align(Alignment.CenterHorizontally))
+                    Text(text = "${index+1}. ilan", style = MaterialTheme.typography.headlineSmall)
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(text = "Bu, ilan açıklaması. Detaylı bilgi için tıklayın.", modifier = Modifier.padding(20.dp))
+
+                }
+            }
+
+            /*Text(
                 text = "Eleman $index",
                 style = MaterialTheme.typography.bodyLarge,
                 modifier = Modifier
@@ -36,6 +71,14 @@ fun LazyColumnContent(
                     .padding(16.dp)
                     .clickable { navController.navigate("announcement_detail/$index") }
             )
+            Text("selma")*/
         }
     }
+}
+
+@Preview
+@Composable
+fun preview(){
+    val navController = rememberNavController()
+    LazyRowContent(navController)
 }
