9d8317b3f398b631f3d545294001f476b2efd108
Onurv77 <ozcanonur1388@hotmail.com>
2024-12-27 02:52:28 +0300

    creatematch

--- app/src/main/java/com/example/santra/ui/components/TopBarContent.kt
+++ app/src/main/java/com/example/santra/ui/components/TopBarContent.kt
@@ -6,14 +6,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
-import androidx.compose.material3.LocalContentColor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -48,7 +45,7 @@ fun TopBarContent() {
                     contentDescription = "Settings",
                     modifier = Modifier
                         .size(60.dp)
-                        .clickable { /* Handle click */ },
+                        .clickable {  },
                     tint = Color.White
                 )
             }
