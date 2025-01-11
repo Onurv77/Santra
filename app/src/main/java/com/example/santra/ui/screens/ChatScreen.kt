be40045d3959535768a6b921426bf2c6dbf0c6ac
DehaTurgan <deha.turgan@std.yeditepe.edu.tr>
2025-01-11 21:40:46 +0300

    Chat Screen Tasarımı

--- app/src/main/java/com/example/santra/ui/screens/ChatScreen.kt
+++ app/src/main/java/com/example/santra/ui/screens/ChatScreen.kt
@@ -1,3 +1,5 @@
+
+
 package com.example.santra.ui.screens
 
 import androidx.compose.foundation.Image
@@ -10,13 +12,16 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
@@ -40,22 +45,41 @@ import com.example.santra.R
 
 @Composable
 fun ChatScreen(navController: NavController) {
-    Scaffold (
+    Scaffold(
         topBar = {
-            TopAppBar(
-                title = { Text("Sohbetler") },
-                backgroundColor = Color(0xFF128C7E),
-                contentColor = Color.White,
-                navigationIcon = {
-                    // Geri dön butonu
-                    IconButton(onClick = { navController.navigateUp()}) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.arrow_left),
-                            contentDescription = "Geri Dön"
-                        )
-                    }
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(80.dp)
+                    .background(Color(0xFF0B4100))
+            ) {
+                // Geri Dön Butonu
+                IconButton(
+                    onClick = { navController.navigateUp() },
+                    modifier = Modifier
+                        .align(Alignment.CenterStart)
+                        .padding(start = 8.dp)
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.arrow_back),
+                        contentDescription = "Geri Dön",
+                        tint = Color.Black,
+                        modifier = Modifier
+                            .height(35.dp)
+                            .width(35.dp)
+                    )
                 }
-            )
+
+                // Başlık
+                Text(
+                    text = "Sohbetlerim",
+                    color = Color.White,
+                    fontSize = 28.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                )
+            }
         },
         content = { paddingValues ->
             Column(
@@ -70,6 +94,11 @@ fun ChatScreen(navController: NavController) {
                             // Sohbete tıklandığında bir mesajlaşma ekranına yönlendirilebilir
                             navController.navigate("message/${chat.id}/${chat.userName}")
                         })
+                        Divider(
+                            color = Color(0xFF000000), // Çizgi rengi
+                            thickness = 1.dp, // Çizgi kalınlığı
+                            //modifier = Modifier.padding(horizontal = 16.dp) // Yatay boşluk
+                        )
                     }
                 }
             }
@@ -83,38 +112,40 @@ fun ChatListItem(chat: ChatItem, onClick: () -> Unit) {
         modifier = Modifier
             .fillMaxWidth()
             .clickable { onClick() }
-            .padding(16.dp),
+            .padding(horizontal = 15.dp, vertical = 10.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
         // Profil Resmi
         Image(
-            painter = painterResource(id = R.drawable.profile),
+            painter = painterResource(id = R.drawable.account_circle),
             contentDescription = "Profil Resmi",
             modifier = Modifier
-                .size(50.dp)
-                .clip(RoundedCornerShape(25.dp))
-                .background(Color.LightGray)
+                .size(100.dp)
+                .clip(RoundedCornerShape(36.dp)),
         )
 
-        Spacer(modifier = Modifier.width(16.dp))
+        Spacer(modifier = Modifier.width(20.dp))
 
         Column(
-            modifier = Modifier.weight(1f)
+            modifier = Modifier
+                .weight(1f)
+                .padding(end = 8.dp)
         ) {
             // Kullanıcı Adı
             Text(
                 text = chat.userName,
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Bold,
+                fontSize = 25.sp,
+                fontWeight = FontWeight.W700,
                 color = Color.Black
             )
             // Son Mesaj
             Text(
                 text = chat.lastMessage,
-                fontSize = 14.sp,
-                color = Color.Gray,
+                fontSize = 16.sp,
+                color = Color(0xFF000000),
                 maxLines = 1,
-                modifier = Modifier.padding(top = 4.dp)
+                modifier = Modifier.padding(top = 6.dp),
+                fontWeight = FontWeight.W500
             )
         }
 
@@ -124,7 +155,7 @@ fun ChatListItem(chat: ChatItem, onClick: () -> Unit) {
         Text(
             text = chat.time,
             fontSize = 12.sp,
-            color = Color.Gray
+            color = Color.Gray,
         )
     }
 }
@@ -138,11 +169,9 @@ data class ChatItem(
 
 // Dummy Chat List
 val dummyChatList = listOf(
-    ChatItem(1, "Ahmet", "Merhaba, nasılsın?", "14:30"),
-    ChatItem(2, "Mehmet", "Bugün görüşelim mi?", "13:45"),
-    ChatItem(3, "Elif", "Teşekkürler!", "12:10"),
-    ChatItem(4, "Zeynep", "Yarınki etkinlik ne zaman?", "09:15"),
-    ChatItem(5, "Ali", "Görüşürüz!", "Dün")
+    ChatItem(1, "Mert", "Merhaba, nasılsın?", "14:30"),
+    ChatItem(2, "Yılmaz", "Bugün", "15:01"),
+    ChatItem(3, "Aslı", "Teşekkürler", "21:05")
 )
 
 @Preview
